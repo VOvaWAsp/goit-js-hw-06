3d5fa979a147225b5aa89b5fa80cbf9b7f8911a1
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-11-09 21:18:31 +0100

    finaly 3

--- js/task-02.js
+++ js/task-02.js
@@ -12,7 +12,11 @@ const listIngredient = document.querySelector("#ingredients")
 const ingredient = [...ingredients];
 
 ingredient.forEach((ingr) => {
+  
   const itemIngredient = document.createElement("li")
+
   itemIngredient.textContent = `${ingr}`;
+
   listIngredient.append(itemIngredient)
+
 })
